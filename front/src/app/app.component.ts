451856ad0d155f90e575346261764432076bdbdf
Thibaud Germain <39822686+Grimnir777@users.noreply.github.com>
2019-12-09 09:35:25 +0100

    Angular Material + first page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'the-best-place';
+  public selected = 'None';
+  tiles: Tile[] = [
+    {text: 'Paris', cols: 1, rows: 2},
+    {text: 'Bordeaux', cols: 1, rows: 2},
+    {text: 'Marseille', cols: 1, rows: 2},
+    {text: 'Nantes', cols: 1, rows: 2},
+    {text: 'Caen', cols: 1, rows: 2},
+    {text: 'Versailles', cols: 1, rows: 2},
+    {text: 'Lyon', cols: 1, rows: 2},
+    {text: 'Perpignan', cols: 1, rows: 2},
+  ];
+}
+
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
 }
