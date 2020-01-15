2cd891af5f5f4aa2c51100e777f65e5bb7345fc8
Thibaud Germain <t.germain2@gmail.com>
2020-01-15 23:33:26 +0100

    Update working

--- scripts/update.py
+++ scripts/update.py
@@ -1,18 +1,16 @@
 from pymongo import MongoClient
 import pandas
+import json
 client = MongoClient('localhost:27017')
 db=client.TheBestPlace.criteres
 
 IN_FILE_PATH = "./scripts/MDB-INSEE-V2.csv"
-search_els = ['CODGEO']
+search_els = ['CODGEO','REG']
 df =  pandas.read_csv(IN_FILE_PATH, sep=';',header=0,usecols = search_els, low_memory = False )
 
-# for i in df.index:
-#     if(i<10):
-#         print(df.loc[i].to_json())
+print('Updating DB')
 
 for index, row in df.iterrows():
-    # db.find_one({'CODGEO':row['CODGEO']})
-    db.update_one({'CODGEO':row['CODGEO']},{'$set': df.loc[index].to_json()}, upsert=False)
+    db.find_one_and_replace({'CODGEO':row['CODGEO']}, json.loads(df.loc[index].to_json()))
 
 print('done')
