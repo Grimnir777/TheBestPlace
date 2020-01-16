2892d7e4ca3eea78f0da9a9a91c2871c8832a11b
Thibaud Germain <t.germain2@gmail.com>
2020-01-16 13:18:28 +0100

    Front : filters css

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule, MatFormFieldModule, MatSelectModule, MatPaginatorModule } from '@angular/material';
+import { 
+  MatGridListModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatPaginatorModule,
+  MatExpansionModule,
+  MatListModule,
+  MatCheckboxModule,
+  MatSliderModule,
+  MatButtonModule,
+  MatCardModule } from '@angular/material';
 import { CityComponent } from './city/city.component';
 import { CitiesComponent } from './cities/cities.component';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -23,7 +33,13 @@ import { HttpClientModule } from '@angular/common/http';
     MatGridListModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatExpansionModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
